cf2c5dc23d051a459fbdd37b2ecacedc11f60d8d
jc <jc@jc>
2018-04-03 09:18:16 -0400

    [chainparams] Update params for BTCP chain

--- src/util.cpp
+++ src/util.cpp
@@ -597,7 +597,7 @@ fs::path GetDefaultDataDir()
     // Unix: ~/.bitcoin
 #ifdef WIN32
     // Windows
-    return GetSpecialFolderPath(CSIDL_APPDATA) / "Bitcoin";
+    return GetSpecialFolderPath(CSIDL_APPDATA) / "BTCP";
 #else
     fs::path pathRet;
     char* pszHome = getenv("HOME");
@@ -607,10 +607,10 @@ fs::path GetDefaultDataDir()
         pathRet = fs::path(pszHome);
 #ifdef MAC_OSX
     // Mac
-    return pathRet / "Library/Application Support/Bitcoin";
+    return pathRet / "Library/Application Support/BTCP";
 #else
     // Unix
-    return pathRet / ".bitcoin";
+    return pathRet / ".btcp";
 #endif
 #endif
 }
